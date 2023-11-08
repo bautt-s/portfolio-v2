dcc4c8c567edbfdb58657c06704ee1c17872f960
bautt-s <bauti_hora@hotmail.com>
2023-11-08 09:44:36 -0300

    fixed analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function Document() {
     return (
@@ -12,7 +11,6 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-                <Analytics />
             </body>
         </Html>
     )
